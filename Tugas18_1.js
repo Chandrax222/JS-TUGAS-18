8e759dd505ac42159400b949fd2fea2beec0b45d
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-26 10:10:45 +0700

    substring
    substring

---
+++ Tugas18_1.js
@@ -0,0 +1,7 @@
+function Tugas18() {
+    var kata = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout";
+
+    console.log(kata.substring(8,24));
+
+}
+Tugas18();
